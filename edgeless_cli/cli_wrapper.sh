163587e16d8073df7706b21f1bdf50640782c962
Raphael Hetzel <hetzel@in.tum.de>
2025-07-15 08:07:07 +0000

    Fix Cli Wrapper

--- edgeless_cli/cli_wrapper.sh
+++ edgeless_cli/cli_wrapper.sh
@@ -2,9 +2,13 @@
 
 # Inspired by the nix tool wrapProgram
 if command -v rustup > /dev/null; then
+    # Nextless requires a nightly compiler for native builds (build_std).
+    # We use a specific nightly here to prevent frequent updates.
     rustup install nightly-2025-07-13 1> /dev/null
     rustup +nightly-2025-07-13 target add wasm32-unknown-unknown 1> /dev/null
-    exec rustup run nightly-2025-07-13 nextless_cli_raw "$@"
+    # rustup run does not work for Cargo used as a crate.
+    export RUSTC="$(rustup +nightly-2025-07-13 which rustc)"
+    exec nextless_cli_raw "$@"
 else
     exec nextless_cli_raw "$@"
 fi
