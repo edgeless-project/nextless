1406b32afa0b6e74b4cb62dd165bba2dc441e90c
Raphael Hetzel <hetzel@in.tum.de>
2026-01-17 22:23:27 +0100

    Add mock sensor value type.

--- edgeless_function_types/src/eval.rs
+++ edgeless_function_types/src/eval.rs
@@ -16,6 +16,17 @@ pub struct EncryptedNumberedTestMessage {
     pub payload: Vec<u8>,
 }
 
+pub struct MockSensorValue {
+    pub sequence_number: u64,
+    pub sensor_id: SensorId,
+    pub value: f64,
+}
+
+pub struct SensorId {
+    pub node_id: [u8; 16],
+    pub component_id: [u8; 16],
+}
+
 impl<'a> edgeless_function_core::Serialize<'a> for NumberedTestMessage {
     fn serialize(&'a self) -> impl core::convert::AsRef<[u8]> {
         let mut out = Vec::with_capacity(self.payload.len() + 8);
@@ -65,6 +76,34 @@ impl<'a> edgeless_function_core::Deserialize<'a> for EncryptedNumberedTestMessag
         Self {
             sequence_number: u64::from_be_bytes(raw[0..8].try_into().unwrap()),
             payload,
+        }
+    }
+}
+
+impl<'a> edgeless_function_core::Serialize<'a> for MockSensorValue {
+    fn serialize(&'a self) -> impl core::convert::AsRef<[u8]> {
+        let mut out = Vec::with_capacity(size_of::<MockSensorValue>());
+        out.extend_from_slice(&self.sequence_number.to_be_bytes());
+        out.extend_from_slice(&self.value.to_be_bytes());
+        out.extend_from_slice(&self.sensor_id.node_id);
+        out.extend_from_slice(&self.sensor_id.component_id);
+        out
+    }
+}
+
+impl<'a> edgeless_function_core::Deserialize<'a> for MockSensorValue {
+    fn deserialize(raw: &'a [u8]) -> Self {
+        assert!(raw.len() >= size_of::<MockSensorValue>());
+
+        let sequence_number = u64::from_be_bytes(raw[0..8].try_into().unwrap());
+        let value = f64::from_be_bytes(raw[8..16].try_into().unwrap());
+        let node_id: [u8; 16] = raw[16..32].try_into().unwrap();
+        let component_id: [u8; 16] = raw[32..48].try_into().unwrap();
+
+        Self {
+            sequence_number,
+            sensor_id: SensorId { node_id, component_id },
+            value,
         }
     }
 }
